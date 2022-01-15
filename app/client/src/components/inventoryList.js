f8d6d8775039f2dd488f3fc712d7215e0fe42122
The Master Dodo <william.wu1219@gmail.com>
2022-01-15 13:30:30 -0500

    remove unused dependency

--- app/client/src/components/inventoryList.js
+++ app/client/src/components/inventoryList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { CSVLink, CSVDownload } from "react-csv";
+import { CSVLink } from "react-csv";
 
 const InventoryItem = (props) => (
   <tr>
